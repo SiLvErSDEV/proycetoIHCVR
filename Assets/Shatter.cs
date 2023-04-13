1b3f421136e3fae7410900b3146cf22916149b32
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-04-13 17:45:07 -0400

    pick up and throw plates to shatter them!
    plates will shatter when, on collision, relative velocity is high enough

---
+++ Assets/Shatter.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shatter : MonoBehaviour
+{
+
+    private bool exploding = false;
+    public GameObject shatteredPrefab;
+    // private void OnTriggerEnter(Collider other) {
+    //     Instantiate(shatteredPrefab, transform.position, transform.rotation);
+    //     Destroy(gameObject);
+    // }
+
+    private void OnCollisionEnter(Collision other) {
+
+        // Debug.Log(other.relativeVelocity.magnitude);
+        // if object velocity is high enough, shatter
+        if (other.relativeVelocity.magnitude > 3) {
+            ShatterObject();
+        }
+
+        // also if object is hit by object with high enough velocity shatter
+        // if (other.gameObject.GetComponent<Rigidbody>() != null) {
+        //     Debug.Log(other.gameObject.GetComponent<Rigidbody>().velocity.magnitude);
+        //     if (other.gameObject.GetComponent<Rigidbody>().velocity.magnitude > 3) {
+        //         ShatterObject();
+        //     }
+        // }        
+    }
+
+
+    public void ShatterObject() {
+        Transform oldRockTransform = transform;
+        Destroy(gameObject);
+
+        if (exploding) {
+            Debug.Log("tried to explode more than once");
+            return;
+        }
+        exploding = true;
+        GameObject explosion = Instantiate(shatteredPrefab, oldRockTransform.position, oldRockTransform.rotation);
+        
+        // Debug.Log(explosion);
+        // Debug.Log(explosion.transform.GetComponentsInChildren<Transform>().Length);
+        // Debug.Log(explosion.transform.childCount);
+
+        
+        
+
+        foreach (Transform g in explosion.transform) {
+            // Debug.Log(g.name);
+            Rigidbody rb = g.GetComponent<Rigidbody>();
+            // rb.AddExplosionForce(force, explosionPosition, explosionRadius, 0, )
+            rb.AddExplosionForce(500f, oldRockTransform.position, 100f);
+        }
+        
+        Destroy(explosion, 3);
+    }
+}
